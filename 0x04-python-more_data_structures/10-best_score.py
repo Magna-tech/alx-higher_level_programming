8ddd5bd6c4ced275d8a912ad13c744c0bc0a2f7b
Nathan Omeri <magembenath@gmail.com>
2022-09-28 19:44:39 -0700

    Get the best scored student

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    else:
+        i = max(a_dictionary.keys(), key = (lambda k: a_dictionary[k]))
+        return i
---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    return (value = lambda x: x * 2, a_dictionary)
