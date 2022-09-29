8eda2f2bd2823f6b3f019a335b77d87a5ee9ab08
Nathan Omeri <magembenath@gmail.com>
2022-09-28 19:47:43 -0700

    Get the best scored student

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     else:
-        i = max(a_dictionary.keys(), key = (lambda k: a_dictionary[k]))
+        i = max(a_dictionary.keys(), key=(lambda k: a_dictionary[k]))
         return i
