be255dfa6f641470c2d3d84af1b5937e57e62e3a
Nathan Omeri <magembenath@gmail.com>
2022-10-11 17:34:19 -0700

    Print x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            n += 1
+        except (TypeError, IndexError):
+            continue
+    print("")
+    return n
