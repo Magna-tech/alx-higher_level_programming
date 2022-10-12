62fa5579aec2fb78c68c4ca74738d829d73a8f27
Nathan Omeri <magembenath@gmail.com>
2022-10-11 18:37:23 -0700

    Print error msg xto stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value), end="\n")
         return True
     except ValueError as ty:
-        print("Exception: {}".format(ty), file=sys.stderr)
+        print("Exception: ", ty, file=sys.stderr)
         return False
