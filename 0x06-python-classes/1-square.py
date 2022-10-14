9200b35deb758572c0662d084b67010c7c50b0bb
Nathan Omeri <magembenath@gmail.com>
2022-10-13 19:00:11 -0700

    Initialize class with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creating a square class"""
+
+
+class Square:
+    """initializing with a private inistance"""
+    def __init__(self, size):
+        """initialize the instance"""
+        self.__size = size
