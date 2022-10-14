a52819d7048b98b39c57fbe8983387c5cc1ab3ef
Nathan Omeri <magembenath@gmail.com>
2022-10-13 19:41:19 -0700

    Print a square of #s fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -18,8 +18,8 @@ class Square:
     def my_print(self):
         for i in range(self.__size):
             for j in range(self.__size):
-                print("#", end="")
-            print("")
+                print("#", end='')
+            print('')
 
     @property
     def size(self):
