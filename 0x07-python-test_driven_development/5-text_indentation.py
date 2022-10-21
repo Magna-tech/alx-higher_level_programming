78e004c821eb65388d2cfd0768f81b5b0dccc35f
Nathan Omeri <magembenath@gmail.com>
2022-10-20 20:40:42 -0700

    Splitting text trial

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    print(text.split("."), end="\n")
