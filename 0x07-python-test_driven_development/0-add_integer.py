5ea1bc07d971406b677d605c685777ba9f8c2ebf
Nathan Omeri <magembenath@gmail.com>
2022-10-24 10:39:10 -0700

    Function documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,6 +3,7 @@
 
 
 def add_integer(a, b=98):
+    """add two integers with b set to a default value"""
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     elif type(b) is not int and type(b) is not float:
