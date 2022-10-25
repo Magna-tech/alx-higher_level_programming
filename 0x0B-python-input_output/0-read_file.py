52154f42da2405865e29db73addccd188fc019d0
Nathan Omeri <magembenath@gmail.com>
2022-10-25 15:51:57 -0700

    Open a file with given name

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This function reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Takes in filename as the parameter"""
+    with open(filename, encoding="utf-8") as new:
+        read_file = new.read()
