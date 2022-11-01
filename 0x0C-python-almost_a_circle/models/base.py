71c5e9f9af13389bed29f3594256a4e694c879a0
Nathan Omeri <magembenath@gmail.com>
2022-10-31 17:32:59 -0700

    Base class for the project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,7 @@ class Base:
     def __init__(self, id=None):
         """Initializing the id value"""
         if id is not None:
-            self.__id = id
+            self.id = id
         else:
-            self.__id = self.__nb_objects++
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
