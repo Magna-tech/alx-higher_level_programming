03527258cb46e46c1f4907d48a7e062d90076769
Nathan Omeri <magembenath@gmail.com>
2022-12-19 19:40:21 -0800

    Add two integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function add(a,b) {
+  return a + b;
+}
+if (process.argv.length < 4) {
+  console.log('NaN');
+} else {
+  console.log(add(~~process.argv[2], ~~process.argv[3]));
+}
