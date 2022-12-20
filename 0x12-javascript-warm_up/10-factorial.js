6398c5987af1ff8139d46abf06a5be34f4d73d8d
Nathan Omeri <magembenath@gmail.com>
2022-12-19 19:54:05 -0800

    Find factorial recursively

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial(a) {
+  if (a === 1) {
+    return 1
+  }
+  return a * factorial(a - 1);
+}
+if (process.argv.length < 3) {
+  console.log(1);
+} else {
+  console.log(factorial(~~process.argv[2]));
+}
