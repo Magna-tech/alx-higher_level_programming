e1b0416c6948736ef3d48b42b23eaadb25e7ab9f
Nathan Omeri <magembenath@gmail.com>
2022-12-19 17:51:28 -0800

    Standard error fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 if (process.argv[2] == null) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (process.argv[3] == null) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
