9b4fc99b0bab3027e91e6d6e276628e8e57ca690
Nathan Omeri <magembenath@gmail.com>
2022-12-21 14:03:34 -0800

    Extend a class with same name as parent.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,5 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const i = this.height;
-    this.height = this.width;
-    this.width = i;
-  }
-
-  double () {
-    this.height *= 2;
-    this.width *= 2;
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c !== undefined) {
       for (let i = 0; i < this.height; i++) {
