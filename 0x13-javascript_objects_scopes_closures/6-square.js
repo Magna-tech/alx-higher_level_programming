488be584da37fd79fcbd8d8a5ef4aeb71235bad4
Nathan Omeri <magembenath@gmail.com>
2022-12-20 20:53:43 -0800

    Print a square of given characters

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 1 && h > 1) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const i = this.height;
+    this.height = this.width;
+    this.width = i;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      return super.print();
+    }
+  }
+}
+module.exports = Square;
