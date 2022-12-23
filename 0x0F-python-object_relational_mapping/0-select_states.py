e048c6f5735987d3a7194187e2d51dddfe96afd5
Nathan Omeri <magembenath@gmail.com>
2022-12-22 18:23:03 -0800

    Ensure the code doesn't run when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,21 +5,26 @@
 import sys
 import MySQLdb
 
-# Connect to the database
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3]
-)
 
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+def main():
+    """contains the whole script"""
+    # Connect to the database
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
 
-cursor.close()
-conn.close()
+
+if __name__ == "__main__":
+    main()
