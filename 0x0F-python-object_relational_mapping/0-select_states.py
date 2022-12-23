aab976abdae00267426a0d54616d33ce6cf0053e
Nathan Omeri <magembenath@gmail.com>
2022-12-22 17:35:13 -0800

    List states from a dbb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+# Connect to the database
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+cursor.close()
+conn.close()
