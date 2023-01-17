7f2263ad6aaea0e3911289e0af164330145534f2
Nathan Omeri <magembenath@gmail.com>
2023-01-17 15:34:13 -0800

    Fetch a page

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+
+req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+with urllib.request.urlopen(req) as result:
+    page = result.read()
+    print("Body Response:")
+    print("\t- type: {}".format(type(page)))
+    print("\t- content: {}".format(page))
+    print("\t- utf8 content: {}".format(page.decode("utf-8")))
