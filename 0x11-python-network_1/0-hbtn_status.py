b3fdce425bcb63c3337b8d85c1210d35dcac7df3
Nathan Omeri <magembenath@gmail.com>
2023-01-17 15:38:22 -0800

    Fetch a page -fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,11 @@
 import urllib.request
 
 
-req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-with urllib.request.urlopen(req) as result:
-    page = result.read()
-    print("Body Response:")
-    print("\t- type: {}".format(type(page)))
-    print("\t- content: {}".format(page))
-    print("\t- utf8 content: {}".format(page.decode("utf-8")))
+if __name__=="__main__":
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as result:
+        page = result.read()
+        print("Body Response:")
+        print("\t- type: {}".format(type(page)))
+        print("\t- content: {}".format(page))
+        print("\t- utf8 content: {}".format(page.decode("utf-8")))
