1e7cf1c11d8c52d32a31a09afe7ba64d0521560b
Nathan Omeri <magembenath@gmail.com>
2023-01-17 17:04:42 -0800

    Return github Id fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,6 +7,6 @@ import requests
 
 if __name__ == "__main__":
     url = "https://api.github.com/user"
-    response = requests.get(url, auth=sys.argv[1], sys.argv[2])
+    response = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
 
     print(response.json().get("id"))
