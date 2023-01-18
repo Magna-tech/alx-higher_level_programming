fa37f8b3f3251d602a89747b92493a672c2ac2b1
Nathan Omeri <magembenath@gmail.com>
2023-01-17 16:22:03 -0800

    Display the body of a page or an error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A Python script that takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8)."""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as page:
+            print(page.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
