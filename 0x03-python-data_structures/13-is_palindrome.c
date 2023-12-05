fb4c09cbb14364896e04d67bb60b7eec1510fc73
Nathan Omeri <magembenath@gmail.com>
2023-12-05 02:23:24 +0000

    Added recursive funtion to check for palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,5 +11,29 @@ int is_palindrome(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return (0);
 
-	return (0);
+	return (p_recursive(head, *head));
+}
+
+/**
+ * p_recursive - Helper function to recursively check palindrome.
+ * @left: Pointer to the left side of the linked list.
+ * @right: Pointer to the right side of the linked list.
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int p_recursive(listint_t **left, listint_t *right)
+{
+	if (right == NULL)
+		return (1);
+
+	int result = p_recursive(left, right->next);
+
+	if (!result)
+		return (0);
+
+	result = (right->n == (*left)->n);
+
+	*left = (*left)->next;
+
+	return (result);
 }
