ad5bc04b289c208ac38accf555c17b4ce8d815e4
Nathan Omeri <magembenath@gmail.com>
2023-12-19 02:58:32 +0000

    Removed for loop

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,10 +3,9 @@ import sys
 
 
 def safe_function(fct, *args):
-    for i in args:
-        try:
-            res = fct(i, i+1,...)
-            return res
-        except IndexError as err:
-            print("Exception: {}".format(err), file=sys.stderr)
-            return None
+    try:
+        res = fct(*args)
+        return res
+    except IndexError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
