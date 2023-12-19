9cfa8c973f3dc375b4dcacb466fa0cfc8dbeee66
Nathan Omeri <magembenath@gmail.com>
2023-12-19 03:11:53 +0000

    Replaced ValueError with Exception to catch all errors

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value), end="\n")
         return True
-    except ValueError as ty:
+    except Exception as ty:
         print("Exception: {}".format(ty), file=sys.stderr, end="\n")
         return False
