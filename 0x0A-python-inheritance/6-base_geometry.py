bcc0a3c02f6384c0488ae66b851239658e487ac2
Nathan Omeri <magembenath@gmail.com>
2024-01-09 01:29:46 +0000

    An area function that raises exceptions

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""An improved class"""
+
+
+class BaseGeometry:
+    """Area function that raises an exception"""
+    def area(self):
+        raise NotImplementedError("area() is not implemented")
