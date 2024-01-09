2e10d1b2cf0f97a799b86e55663df529c431fa16
Nathan Omeri <magembenath@gmail.com>
2024-01-09 01:00:38 +0000

    Check if an instance belongs to the same class or one that inherited from it

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A function that checks if an instance is same class or inherited"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Return true if object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False
+    """
+    return isinstance(obj, a_class) or issubclass(type(obj), a_class)
