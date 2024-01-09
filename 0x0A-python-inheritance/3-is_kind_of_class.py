334ca98885b31714f5b22e24e7d079c79375f817
Nathan Omeri <magembenath@gmail.com>
2024-01-09 01:03:02 +0000

    Function definition fix

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,7 +2,7 @@
 """A function that checks if an instance is same class or inherited"""
 
 
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """
     Return true if object is an instance of,
     or if the object is an instance of a class that inherited from,
