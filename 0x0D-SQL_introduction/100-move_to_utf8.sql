9f3c5902cc3c493159d7438aecb125ebd0e821c8
Nathan Omeri <magembenath@gmail.com>
2024-02-13 13:27:19 +0000

    Convert database to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,4 +8,4 @@ ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode
 -- Convert field
 ALTER TABLE first_table
 	DEFAULT CHARACTER SET utf8mb4,
-	MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+	MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL;
