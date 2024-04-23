c12de1c9e572b23a0cbb162a50d1c660c14ff4be
Nathan Omeri <magembenath@gmail.com>
2024-04-23 21:44:08 +0000

    A script that writes to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if file path and string to write arguments are provided
+if (process.argv.length < 4) {
+  console.error('Usage: node write_file.js <file_path> <string_to_write>');
+  process.exit(1);
+}
+
+// Get file path and string to write from command line arguments
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+// Write string to file
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Successfully wrote "${stringToWrite}" to ${filePath}`);
+  }
+});
