d8cf4660c283c62884305566e7fa42fcb1be289f
Nathan Omeri <magembenath@gmail.com>
2024-04-23 21:38:22 +0000

    Script that displays the contents of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+
+// Check if file path is provided
+if (process.argv.length < 3) {
+  console.error('Usage: node read_file.js <file_path>');
+  process.exit(1);
+}
+
+// Get file path from command line argument
+const filePath = process.argv[2];
+
+// Read file asynchronously
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
+
